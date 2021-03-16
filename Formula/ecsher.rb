7abe9d246c88b9da719b259aac2e70f2c0fc6638
mic_u666 <yuho.miki@gmail.com>
2021-03-17 08:34:32 +0900

    v0.3.1

--- Formula/ecsher.rb
+++ Formula/ecsher.rb
@@ -1,9 +1,9 @@
 class Ecsher < Formula
     desc "CLI tool describing ECS resources like kubectl written in Go"
     homepage "https://github.com/Mic-U/ecsher"
-    url "https://github.com/Mic-U/ecsher/releases/download/v0.3.0/ecsher_0.3.0_Darwin_x86_64.tar.gz"
-    sha256 "4bab0d8b323a5318f1f45ba8bc6250f115e00f63c86959bfe3de23b4fe044d0f"
-    version "0.3.0"
+    url "https://github.com/Mic-U/ecsher/releases/download/v0.3.1/ecsher_0.3.1_Darwin_x86_64.tar.gz"
+    sha256 "bd433cc0e45067638c8a87dd2bd6a9889948ba76dc355e8553585d61e23f6f18"
+    version "0.3.1"
 
     def install
         bin.install "ecsher"
